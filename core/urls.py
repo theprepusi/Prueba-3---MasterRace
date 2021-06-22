695c0ac2a6a5e3ce626fb8b2440aa625ca058db3
esotik <eli.beltran@duocuc.cl>
2021-06-21 22:51:36 -0400

    ++

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import  home, login
+from core.views import  cpu, gpu, hdd, home, login, m2, mb, psu, ram, signup, ssd
 
 urlpatterns = [
     path('', home, name='home'),
     path('login', login, name='login'),
+    path('signup', signup, name='signup'),
+    path('cpu', cpu, name='cpu'),
+    path('gpu', gpu, name='gpu'),
+    path('hdd', hdd, name='hdd'),
+    path('m2', m2, name='m2'),
+    path('mb', mb, name='mb'),
+    path('psu', psu, name='psu'),
+    path('ram', ram, name='ram'),
+    path('ssd', ssd, name='ssd'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,30 @@ def home(request):
 
 def login(request):
     return render(request, 'login.html', {})
+
+def signup(request):
+    return render(request, 'signup.html', {})
+
+def cpu(request):
+    return render(request, 'cpu.html', {})
+
+def gpu(request):
+    return render(request, 'gpu.html', {})
+
+def hdd(request):
+    return render(request, 'hdd.html', {})
+
+def m2(request):
+    return render(request, 'm2.html', {})
+
+def mb(request):
+    return render(request, 'mb.html', {})
+
+def psu(request):
+    return render(request, 'psu.html', {})
+
+def ram(request):
+    return render(request, 'ram.html', {})
+    
+def ssd(request):
+    return render(request, 'ssd.html', {})                    
